28093bce52fa1071fc1940bbc9c2341aea64ae5d
Trayden <traybouc@hotmail.com>
2023-03-31 04:47:30 -0400

    comments

--- Napoleon/Core/civictree_db.sql
+++ Napoleon/Core/civictree_db.sql
@@ -2,6 +2,7 @@
 -- Author: trayb
 -- DateCreated: 3/31/2023 3:09:07 AM
 --------------------------------------------------------------
+-- add Mercenaries description to DB (not in by default) before changing the text
 UPDATE Civics
 SET Description = 'LOC_CIVIC_MERCENARIES_DESCRIPTION'
 WHERE CivicType = 'CIVIC_MERCENARIES';
--- Napoleon/Core/civictree_text.sql
+++ Napoleon/Core/civictree_text.sql
@@ -2,12 +2,12 @@
 -- Author: trayb
 -- DateCreated: 3/31/2023 2:35:40 AM
 --------------------------------------------------------------
--- TO FIX NATIONALISM USUALLY SHOWING CORPS
--- insert text for mercenaries to add corps part
+-- change Nationalism text to mention Napoleon Bonaparte's Corps ability at Mercenaries
 UPDATE LocalizedText
 SET Text = 'Grants the ability to construct an additional Spy. Allows forming two identical units into a Corps or Fleet. Allows one new Casus Belli that can be used to justify wars: Colonial War.[NEWLINE][NEWLINE]Napoleon Bonaparte can form Corps earlier, at Mercenaries.'
 WHERE Tag = 'LOC_CIVIC_NATIONALISM_DESCRIPTION';
 
+-- change Mercenaries text to include Napoleon Bonaparte's Corps ability
 UPDATE LocalizedText
 SET Text = 'If playing as Napoleon Bonaparte, allows forming two identical units into a Corps.'
 WHERE Tag = 'LOC_CIVIC_MERCENARIES_DESCRIPTION';
